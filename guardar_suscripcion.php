07ea4cf1750f12b6f318f604c19a1a1232f8bdb6
ingsebas724 <ingsebastianpardo@gmail.com>
2025-01-30 18:03:47 -0500

    decimo

---
+++ guardar_suscripcion.php
@@ -0,0 +1,36 @@
+<?php
+// Configuración de la base de datos
+$servername = "Local instance MySQL80"; // Servidor de la base de datos
+$username = "root"; // Usuario de la base de datos
+$password = "123Sebas."; // Contraseña de la base de datos
+$dbname = "suscripciones"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+// Obtener el correo electrónico del formulario
+$email = $_POST['email'];
+
+// Validar el correo electrónico
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Preparar la consulta SQL
+    $sql = "INSERT INTO suscriptores (email) VALUES ('$email')";
+
+    // Ejecutar la consulta
+    if ($conn->query($sql) === TRUE) {
+        echo "¡Gracias por suscribirte!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    echo "Por favor, ingresa un correo electrónico válido.";
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
